f55f49b56e07bcd0e832bddf71f9aa107b27cd61
Dan Payne <dpayne@Dans-MacBook-Pro.local>
2022-03-26 13:01:03 -0600

    Initial Commit

---
+++ I AM FUCKING RICH/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I AM FUCKING RICH
+//
+//  Created by Dan Payne on 3/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
